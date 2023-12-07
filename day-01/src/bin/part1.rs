40ea0cc8cf6cf025e1ce4da60676e31359ea26fa
Antal Kenderesi <43848302+alkenderesi@users.noreply.github.com>
2023-12-07 17:13:51 +0100

    Add day 01 part 1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -5,5 +5,42 @@ fn main() {
 }
 
 fn part1(input: &str) -> String {
-    input.to_string()
+
+    // Split the input into multiple lines
+    let calibration_document = input.lines();
+
+    // Extract the values from each line and sum them up
+    let calibration_value: u32 = calibration_document.map(extract_value).sum();
+
+    // Return the solution as a string
+    calibration_value.to_string()
+}
+
+fn extract_value(line: &str) -> u32 {
+    let mut first_digit: u32 = 0;
+    let mut last_digit: u32 = 0;
+
+    // Search forwards
+    for chr in line.chars() {
+        match chr.to_digit(10) {
+            Some(digit) => {
+                first_digit = digit;
+                break;
+            },
+            None => {}
+        }
+    }
+
+    // Search backwards
+    for chr in line.chars().rev() {
+        match chr.to_digit(10) {
+            Some(digit) => {
+                last_digit = digit;
+                break;
+            },
+            None => {}
+        }
+    }
+
+    first_digit * 10 + last_digit
 }
